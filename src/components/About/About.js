007a504a82cacfd71bb4c34a452cf67b01c67244
mukarrom21 <mukarrom21@gmail.com>
2022-04-27 10:24:47 +0600

    commit to push

---
+++ src/components/About/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h1>We are secrete</h1>
+            <h1>no one know About Us</h1>
+        </div>
+    );
+};
+
+export default About;
---
+++ src/components/Orders/Orders.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import useCart from '../../hooks/useCart';
+import useProducts from '../../hooks/useProducts';
+import Cart from '../Cart/Cart';
+
+const Orders = () => {
+    const [products, setProducts] = useProducts();
+    const [cart, setCart] = useCart(products);
+    return (
+        <div className='shop-container'>
+            <div className="products-container">
+
+            </div>
+            <div className="cart-container">
+                <Cart cart={cart}></Cart>
+            </div>
+        </div>
+    );
+};
+
+export default Orders;
